e661808a7dffa68fb18952fbf9177868d230e13c
Kyle Mayes <kyle@mayeses.com>
2017-04-16 18:35:41 -0400

    Rework scripts

--- build.sh
+++ build.sh
@@ -1,11 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-aws s3 cp s3://sekure-archive-ec2/psql.sh .
-chmod +x psql.sh
+# Stop the current server container, if any
+CONTAINER_ID=$(sudo docker ps --format "{{.ID}}")
+if [[ $CONTAINER_ID ]]; then
+    sudo docker kill $CONTAINER_ID
+    sudo docker rm $CONTAINER_ID
+fi
+
+sudo aws s3 cp s3://sekure-archive-ec2/psql.sh .
+sudo chmod +x psql.sh
+
 sudo apt install -y awscli docker.io
 sudo systemctl start docker.service
-git clone https://github.com/SEKure-archive/sekure-archive-server
+
+sudo git clone https://github.com/SEKure-archive/sekure-archive-server
 cd sekure-archive-server
+sudo git pull origin master
+
+# Build the server container
 sudo docker build -t sekure-archive-server .
-#sudo docker run -p 80:80 sekure-archive-server
+
+# Delete unused images
+IMAGE_IDS=$(sudo docker images --filter "dangling=true" -q --no-trunc)
+if [[ $IMAGE_IDS ]]; then
+    sudo docker rmi $IMAGE_IDS
+fi
+
+# Run the server container
 sudo docker run sekure-archive-server
--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-# Store config files in s3 bucket
-s3bucketName="sekure-archive-ec2"
+# The S3 bucket containing EC2 related configuration files
+S3_BUCKET="sekure-archive-ec2"
 
+# Install Nginx if it is not already installed
 if [ ! -x /usr/sbin/nginx ]; then
-    echo "Nginx not installed"
-    echo "Installing Nginx"
-    apt-get update -y
-    apt-get install nginx -y
-    ufw app list
-    ufw allow 'Nginx HTTPS'
-  else
-    echo "Nfinx already installed"
+    echo "Nginx not installed, installing..."
+    sudo apt-get update -y
+    sudo apt-get install -y nginx
+    sudo ufw app list
+    sudo ufw allow 'Nginx HTTPS'
+    sudo useradd --no-create-home nginx
+else
+    echo "Stopping Nginx..."
+    sudo systemctl stop nginx
 fi
-systemctl stop nginx
-echo "Preparing File permissions..."
-useradd --no-create-home nginx
-chown -R root:root .
-chmod -R 0755 html
 
-echo "Installing Files..."
-rsync -a html/ /usr/share/nginx/html/
-aws s3 cp "s3://${s3bucketName}/default.conf" /etc/nginx/conf.d/default.conf
-systemctl start nginx
-echo "Starting Nginx Sever..."
+# Install static files
+echo "Setting static file permissions..."
+sudo chown -R root:root .
+sudo chmod -R 0755 html
+echo "Copying files..."
+sudo rsync -a html/ /usr/share/nginx/html/
+sudo aws s3 cp "s3://${S3_BUCKET}/default.conf" /etc/nginx/conf.d/default.conf
 
+# Start Nginx
+echo "Starting Nginx server..."
+sudo systemctl start nginx
 
-echo "Building API..."
-sh build.sh
+# Compile and start Express server
+echo "Building Express server..."
+bash build.sh
